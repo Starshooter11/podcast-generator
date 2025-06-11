7d06ccabb96cd6e1945a3ebdc712feb4903da388
Starshooter11 <112408887+Starshooter11@users.noreply.github.com>
2025-06-11 20:48:08 +0000

    entrypoint again

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,3 +17,8 @@ echo "======================="
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directry /github/workspace
+
+python3 /usr/bin/feed.py
+
+git add -A && git commit -m "Update Feed"
+git push --set-upstream origin main
